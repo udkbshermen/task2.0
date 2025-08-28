5c298467af37eaeb5219115365c78f94fa729444
udkbshermen <shyrmin.zoobooksystems@gmail.com>
2025-08-28 05:19:02 +0000

    task 2.0 output

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        boolean notFalse = true;
+        byte l = 1;
+        short o = 0;
+        int e = 3;
+        float two = 2.0f;
+        char d = 'd';
 
+        System.out.println("H"+ e + l + l + o + " w" + o + "r" + l + d + " " + two + " " + notFalse);
+    }
+}
